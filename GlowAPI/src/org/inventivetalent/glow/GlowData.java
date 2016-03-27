5a7e793545ff5a419a0ae21aaa06b6fd7d05a4e6
inventivetalent <inventivetalent@inventivegames.de>
2016-03-27 13:46:43 +0200

    add equals & hashCode to glow data

--- GlowAPI/src/org/inventivetalent/glow/GlowData.java
+++ GlowAPI/src/org/inventivetalent/glow/GlowData.java
@@ -9,4 +9,19 @@ public class GlowData {
 	//Maps player-UUID to Color
 	public Map<UUID, GlowAPI.Color> colorMap = new HashMap<>();
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) { return true; }
+		if (o == null || getClass() != o.getClass()) { return false; }
+
+		GlowData glowData = (GlowData) o;
+
+		return colorMap != null ? colorMap.equals(glowData.colorMap) : glowData.colorMap == null;
+
+	}
+
+	@Override
+	public int hashCode() {
+		return colorMap != null ? colorMap.hashCode() : 0;
+	}
 }
