e77a2a93deb8af8a221040d9cc1bdcbf6da0d23d
inventivetalent <mail@inventivetalent.org>
2022-09-15 08:03:04 +0200

    remove my very professional testing code

--- src/org/inventivetalent/glow/GlowPlugin.java
+++ src/org/inventivetalent/glow/GlowPlugin.java
@@ -2,13 +2,8 @@ package org.inventivetalent.glow;
 
 import org.bstats.bukkit.MetricsLite;
 import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.inventivetalent.packetlistener.PacketListenerAPI;
-
-import java.util.Random;
 
 public class GlowPlugin extends JavaPlugin {
 
@@ -27,15 +22,6 @@ public class GlowPlugin extends JavaPlugin {
         PacketListenerAPI.addPacketHandler(glowAPI);
 
         new MetricsLite(this, 2190);
-
-        Bukkit.getPluginManager().registerEvents(new Listener() {
-
-            @EventHandler
-            public void on(PlayerInteractEvent event) {
-                GlowAPI.setGlowing(event.getPlayer(), GlowAPI.Color.values()[new Random().nextInt(GlowAPI.Color.values().length)], Bukkit.getOnlinePlayers());
-            }
-
-        }, this);
     }
 
     @Override
